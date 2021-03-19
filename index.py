b66e144f7d464f0a851e1a8bc35248e390bb8bcc
FredysMuriel <75293790+FredysMuriel@users.noreply.github.com>
2021-03-18 23:08:07 -0500

    Correpción del index con proporcionalidades en windows

--- index.py
+++ index.py
@@ -5,6 +5,10 @@ def exit():
 	root.destroy()
 	import login.py
 
+def bookings():
+	root.destroy()
+	import bookings.py
+
 title = ("Arial", 20)
 font_base = ("Arial", 18)
 font_answers = ("Arial", 15)
@@ -20,18 +24,16 @@ tk.Button(root, text="Salir", font=font_base, bg="#eccc68", command=exit).place(
 tk.Label(root, text='Bienvenido '+" @Username", bg="#dfe4ea", font=font_answers).place(x=20, y=100)
 tk.Label(root, text='Opciones: ', bg="#dfe4ea", font=font_answers).place(x=20, y=150)
 rImg = tk.PhotoImage(file="img/stock.png")
-tk.Button(root, text="Revisar Reservas", font=font_answers, bg="#eccc68").place(x=20, y=200)
+tk.Button(root, text="Revisar Reservas", font=font_answers, bg="#eccc68", command=bookings).place(x=20, y=200, width=180)
 tk.Label(root, image=rImg, bg="#eccc68").place(x=202, y=202, height=35)
 dImg = tk.PhotoImage(file="img/remove.png")
-tk.Button(root, text="Eliminar Reservas", font=font_answers, bg="#eccc68").place(x=20, y=250)
+tk.Button(root, text="Eliminar Reservas", font=font_answers, bg="#eccc68").place(x=20, y=250, width=180)
 tk.Label(root, image=dImg, bg="#eccc68").place(x=204, y=252, height=35)
 sImg = tk.PhotoImage(file="img/stock.png")
-tk.Button(root, text="Registro Extendido", font=font_answers, bg="#eccc68").place(x=20, y=300)
+tk.Button(root, text="Registro Extendido", font=font_answers, bg="#eccc68").place(x=20, y=300, width=180)
 tk.Label(root, image=sImg, bg="#eccc68").place(x=204, y=302, height=35)
 fImg = tk.PhotoImage(file="img/invoice.png")
-tk.Button(root, text="Facturación", font=font_answers, bg="#eccc68").place(x=20, y=350, width=200)
+tk.Button(root, text="Facturación", font=font_answers, bg="#eccc68").place(x=20, y=350, width=180)
 tk.Label(root, image=fImg, bg="#eccc68").place(x=204, y=352, height=35)
-
-
 
 root.mainloop()
